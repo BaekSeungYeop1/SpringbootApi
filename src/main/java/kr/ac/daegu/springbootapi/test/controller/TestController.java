4d11bf991689f2fd1417d5a2e8b063deda0b4173
baekseungyeop <tmdduq5498@gmail.com>
2021-09-28 22:39:05 +0900

    test/ 수정

--- src/main/java/kr/ac/daegu/springbootapi/test/controller/TestController.java
+++ src/main/java/kr/ac/daegu/springbootapi/test/controller/TestController.java
@@ -5,11 +5,13 @@ import kr.ac.daegu.springbootapi.test.service.TestService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping(value = "/test")
 @RequiredArgsConstructor
 @Slf4j
 public class TestController {
