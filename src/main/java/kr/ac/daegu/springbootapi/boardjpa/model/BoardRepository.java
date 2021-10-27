834d688513306461558b66f694cb8177ae230d57
baekseungyeop <tmdduq5498@gmail.com>
2021-10-27 18:15:20 +0900

    GET /boardjpa/ Paging 기본 - 0페이지, 페이지당 size 5개 나오도록

--- src/main/java/kr/ac/daegu/springbootapi/boardjpa/model/BoardRepository.java
+++ src/main/java/kr/ac/daegu/springbootapi/boardjpa/model/BoardRepository.java
@@ -1,16 +1,19 @@
 package kr.ac.daegu.springbootapi.boardjpa.model;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 
-public interface BoardRepository extends CrudRepository<Board, Integer> {
+public interface BoardRepository extends JpaRepository<Board, Integer> {
     List<Board> findAll();
 
     Board findBoardById(Integer id);
     Optional<Board> findBoardById(int id);
 
-    List<Board> findBoardByIsDel(String isDel);
+    Page<Board> findBoardsByIsDel(String isDel, Pageable pageable);
 }
